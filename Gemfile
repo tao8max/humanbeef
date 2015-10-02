ba29af26e6a6e44e551d7b8de1e585931611b5bb
Maxim Stoupnikov <taomax8@gmail.com>
2015-10-02 10:44:08 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'   
+ruby '2.2.1'
+
+gem 'mandrill-api'
+gem 'sinatra'
---
+++ index.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'mandrill' 
+
+get '/' do     
+	erb :index   
+end
+get '/about' do 
+	erb :about
+end
+get '/products' do
+	erb :products
+end
+
+get '/faq' do
+	erb :faq
+end
+
+get '/contact' do
+	erb :contact
+end
