ad761e3a014b331f18216b93fa06f9bac18e0fbe
John Paul Tambini <Tambini.jp@gmail.com>
2015-10-04 22:11:43 -0400

    added multiple pictures to recipes page and centered pictures.

--- public/recipes.js
+++ public/recipes.js
@@ -1,12 +1,26 @@
 
 $(document).ready(function(){
 
-	$("#image1").click(function(){
-		$("#rumproastrecipe").fadeOut();
-		
+	$("#rumproast").click(function(){
+		$("#rumproastrecipe").slideToggle();
 	});
-		
+	$("#ribs").click(function(){
+		$("#ribsrecipe").slideToggle();
+	});
+	$("#chitlins").click(function(){
+		$("#chitlinsrecipe").slideToggle();
+	});
+	$("#schnitzel").click(function(){
+		$("#schnitzelrecipe").slideToggle();
+	});
 
+
+	// $("#image1").click(function(){
+	// var effect = 'slide';
+ //    var slideLeft = { direction: $("slideLeft").val()};
+ //    var duration = 500;
+ //    $("#rumproastrecipe").toggle(effect, slideLeft, duration);
+	// });
 
 
 });
