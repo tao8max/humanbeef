a31a5b13bd262c7f2865a3168878f9a03ceb64ce
taomax <taomax8@gmail.com>
2015-10-05 10:13:52 -0400

    Revert "added multiple pictures to recipes page and centered pictures."

--- public/recipes.js
+++ public/recipes.js
@@ -1,26 +1,12 @@
 
 $(document).ready(function(){
 
-	$("#rumproast").click(function(){
-		$("#rumproastrecipe").slideToggle();
+	$("#image1").click(function(){
+		$("#rumproastrecipe").fadeOut();
+		
 	});
-	$("#ribs").click(function(){
-		$("#ribsrecipe").slideToggle();
-	});
-	$("#chitlins").click(function(){
-		$("#chitlinsrecipe").slideToggle();
-	});
-	$("#schnitzel").click(function(){
-		$("#schnitzelrecipe").slideToggle();
-	});
+		
 
-
-	// $("#image1").click(function(){
-	// var effect = 'slide';
- //    var slideLeft = { direction: $("slideLeft").val()};
- //    var duration = 500;
- //    $("#rumproastrecipe").toggle(effect, slideLeft, duration);
-	// });
 
 
 });
