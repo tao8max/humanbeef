79425999aaa66369b6d77065b8afe5c865eb8c7b
Maxim Stoupnikov <taomax8@gmail.com>
2015-10-05 09:52:14 -0400

    Added product page and images

--- main.rb
+++ main.rb
@@ -20,3 +20,25 @@ end
 get '/contact' do
 	erb :contact
 end
+
+def send_email(message_body)
+  
+  mandrill = Mandrill::API.new
+
+  message = {
+   :subject => "Hello from the Mandrill API",
+   :from_name => name,
+   :text => message_body,
+   :to => [{:email=> "taomax8@gmail.com", :name=> "Max"}],
+   :html => "<html><h1>#{message_body}</h1></html>",
+   :from_email => email
+  }
+
+  sending = mandrill.messages.send message
+  puts sending
+end
+
+post "/contact_us_post" do 
+	puts "Sending email now"
+	send_email params[:message_body]
+end
